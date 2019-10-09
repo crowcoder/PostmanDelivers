f5621a5255ddda7f698e2d4306890b54d9366566
Bob Crowley <bcrowley@stone-coast.com>
2019-10-09 13:09:55 -0400

    added Echo controller

---
+++ PostmanDelivers.API/Controllers/EchoController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace PostmanDelivers.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EchoController : ControllerBase
+    {
+        [HttpPost]
+        [Consumes("application/x-www-form-urlencoded")]
+        public ActionResult Post([FromForm] Dictionary<string, string> stringsToEcho)
+        {
+            return Ok(stringsToEcho);
+        }
+    }
+}
